537f3b78f60a9c3cfb086265074f44ce345e1755
dfermic <fermicdamir@gmail.com>
2024-04-02 21:38:47 +0200

    Lesson two changes

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IPost } from 'src/app/models/IPost';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css']
+  styleUrls: ['./post.component.css'],
 })
 export class PostComponent {
-
+  @Input()
+  post: IPost = {} as IPost;
 }

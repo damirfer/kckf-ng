860cbd5bf95f11dd702375594d59334fbcd7881c
dfermic <damir.fermic@swissfex.ch>
2024-04-08 23:11:54 +0200

    Post service

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { IPost } from 'src/app/models/IPost';
+import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-home',
@@ -8,26 +9,10 @@ import { IPost } from 'src/app/models/IPost';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  posts: IPost[] = [
-    {
-      id: 1,
-      title:
-        'TechCrunch Minute: Robinhood’s credit card has arrived to take on Apple',
-      body: 'But what gives with tech companies getting into the consumer credit game? You could argue that Robinhood’s choice to offer a card is just an extension of its already-expanding portfolio of financial products. But Apple also has a card, recall. And the tech giant is getting deeper into the realm of personal finance as time goes along.',
-      imgUrl:
-        'https://techcrunch.com/wp-content/uploads/2024/03/greysen-johnson-cZVzzFadTMc-unsplash.jpg?w=1390&crop=1',
-    },
-    {
-      id: 2,
-      title:
-        'Google will let you swipe right or left on clothes to get better fashion recommendations',
-      body: 'Google is rolling out a new update that makes it easier for users to find personalized shopping results, the company announced on Wednesday. The new feature lets users rate different products in order to get style recommendations when shopping for apparel, shoes and accessories. The update comes as Google continues to compete with companies like Amazon and Walmart',
-      imgUrl:
-        'https://techcrunch.com/wp-content/uploads/2021/07/GettyImages-1207206237.jpg?w=1390&crop=1',
-    },
-  ];
+  posts: IPost[] = [];
 
-  constructor(private router: Router) {
+  constructor(private postService: PostService, private router: Router) {
+    this.posts = this.postService.getPosts();
   }
 
   navigateToSinglePost(post: IPost) {
--- src/app/pages/single-post/single-post.component.ts
+++ src/app/pages/single-post/single-post.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { IPost } from 'src/app/models/IPost';
+import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-single-post',
@@ -8,12 +10,16 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class SinglePostComponent implements OnInit {
   id: string = '';
-
-  constructor(private route: ActivatedRoute) { }
+  post: IPost = {} as IPost;
+  constructor(private route: ActivatedRoute, private postService: PostService) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.id = params['id'];
-    });
+    });    
+  }
+
+  loadPost() {
+    this.post = this.postService.getSinglePost(parseInt(this.id));
   }
 }
